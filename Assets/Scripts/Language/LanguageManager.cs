d77c443b076ba9592e7d59e34edc930909f097d8
Aluno <aluno@naverecife.local>
2019-01-31 15:11:24 -0300

    Slide Settings

---
+++ Assets/Scripts/Language/LanguageManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguageManager : MonoBehaviour
+{   
+
+    public static int language;
+    
+    /*
+        Language Index:
+        0 - English
+        1 - Portuguese
+     */
+
+    void Awake()
+    {
+        if(PlayerPrefs.HasKey("Language")){
+            PlayerPrefs.SetInt("Language", 0);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public Animator anim;
+	public Animator anim2;
+    
+    public void ChangeScene(string name){
+        SceneManager.LoadScene(name);
+    }
+
+    public void TransitionToSettings(){
+        StartCoroutine(SettingsScene());
+    }
+
+    public void TransitionToMenu(){
+        StartCoroutine(MenuScene());
+    }
+
+    IEnumerator SettingsScene(){       
+
+        anim.SetBool("glitching",true);
+		anim2.SetBool("glitching",true);
+        yield return new WaitForSeconds(0.1f);
+        AudioManager.instance.Play("Transition");
+        yield return new WaitForSeconds(1.7f);
+        SceneManager.LoadScene("Settings");
+    }
+
+    IEnumerator MenuScene(){
+        anim.SetBool("glitching",true);
+		anim2.SetBool("glitching",true);
+        yield return new WaitForSeconds(0.1f);
+        AudioManager.instance.Play("Transition");
+        yield return new WaitForSeconds(1.7f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
