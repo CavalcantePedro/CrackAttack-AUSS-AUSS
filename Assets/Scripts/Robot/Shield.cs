f1c1339cb5845da7d6ed0620205d7baa9ed48287
Aluno <aluno@naverecife.local>
2019-02-21 18:24:34 -0300

    Shield 0.2

--- Assets/Scripts/Robot/Shield.cs
+++ Assets/Scripts/Robot/Shield.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    [SerializeField] private float shrinkingSpeed;
+    [SerializeField] private float expandingDelay;
+
+    private float xGrow;
+    private float yGrow;
+
+    private int axisExpandRate;
+
+    private CircleCollider2D myCollider;
 
     void Start()
     {
-        
+        StartCoroutine(ExpandingShield());
+        myCollider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+     DeactivatingCollisions();
+     ConstantShrinking(); 
+    }
+
+    void ConstantShrinking()
+    {
+        if(transform.localScale.x > 0.5f)
+        {
+            transform.localScale = new Vector2(transform.localScale.x - shrinkingSpeed, transform.localScale.y);
+        }
+
+        if(transform.localScale.y > 0.5f)
+        {
+            transform.localScale = new Vector2(transform.localScale.x , transform.localScale.y - shrinkingSpeed);
+        }
+    }
+
+    void DeactivatingCollisions()
+    {
+        if(transform.localScale.x <= 0.5 && transform.localScale.y <= 0.5)
+        {
+            myCollider.enabled = false;
+        }
+
+        else
+        {
+            myCollider.enabled = true;
+        }
+
+    }
+
+    void GeneratingExpandValues()
+    {
+        axisExpandRate = Random.Range(0,100);
+        xGrow = Random.Range(1f,3f);
+        yGrow = Random.Range(1f,3f);
+    }
+        
+
+    IEnumerator ExpandingShield()
+    {
+        for(;;)
+        {
+            GeneratingExpandValues();
+            if(axisExpandRate <= 60)
+                {
+                     transform.localScale = new Vector2(xGrow,yGrow);   
+                }
+
+            else if (axisExpandRate <= 80 && axisExpandRate > 60)
+                {
+                    transform.localScale = new Vector2(xGrow,transform.localScale.y);
+                }
+
+            else if (axisExpandRate <= 100 && axisExpandRate > 80)
+                {
+                    transform.localScale = new Vector2(transform.localScale.x,yGrow);
+                }
+    
+        yield return new WaitForSeconds(expandingDelay);
+        }
     }
 }
+
