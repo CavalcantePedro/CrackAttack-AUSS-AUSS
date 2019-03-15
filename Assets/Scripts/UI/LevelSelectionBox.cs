0291d0011fba5837fccbb90f63e9398c70a9e84d
Aluno <aluno@naverecife.local>
2019-03-15 18:52:22 -0300

    Interface 2.0

---
+++ Assets/Scripts/UI/LevelSelectionBox.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectionBox : MonoBehaviour
+{
+    
+    [Header("Level Numbers")]
+
+    [SerializeField] private Image numberUI;
+    [SerializeField] private Sprite[] numberSprites;
+    [SerializeField] private Animator numberAnim;
+    [SerializeField] private GameObject plusOneBtn;
+    [SerializeField] private GameObject minusOneBtn;
+    private int levelNumber;
+
+    void Start()
+    {
+        levelNumber = 1;
+        ManagingLevels();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SelectNextLevel()
+    {
+        levelNumber++;
+        ManagingLevels();
+    }
+
+    public void SelectPreviousLevel()
+    {
+        levelNumber--;
+        ManagingLevels();
+    }
+
+    void ManagingLevels()
+    {
+        numberUI.sprite = numberSprites[levelNumber];
+        print("a");
+        numberAnim.SetInteger("TransitionTo",levelNumber);
+        switch(levelNumber)
+        {
+        case 1 :
+        minusOneBtn.SetActive(false);
+        break;
+
+        case 4 :
+        plusOneBtn.SetActive(false);
+        break;
+
+        default:
+        if(!minusOneBtn.activeSelf || !plusOneBtn.activeSelf)
+        {
+        minusOneBtn.SetActive(true);
+        plusOneBtn.SetActive(true);
+        }
+        break;
+        }
+
+    }
+}
