db805dc1e2f8021348f432baad05823f3435d7f8
Aluno <aluno@naverecife.local>
2019-03-15 18:31:46 -0300

    LevelSelectScript_0.1

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager instance;
+	public Sound[] sounds;
+
+	void Awake() {
+		if(instance == null){
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else{
+			Destroy(gameObject);
+		}
+		SetAudio();
+	}
+
+	void SetAudio(){
+		foreach(Sound s in sounds){
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.audio;
+			s.source.volume = s.volume;
+			s.source.playOnAwake = s.PlayOnAwake;
+			s.source.loop = s.loop;
+			if(s.PlayOnAwake) s.source.Play();
+		}
+	}
+
+	public void Play(string name){
+		Sound s = Array.Find(sounds, sound => sound.name == name);
+		if(s == null){
+			Debug.LogWarning("Sound: " + name + "doesn't exist!");
+			return;
+		}
+		s.source.Play();
+       
+	}
+
+	public void Pause(string name){
+		Sound s = Array.Find(sounds, sound => sound.name == name);
+		if(s == null){
+			Debug.LogWarning("Sound: " + name + "doesn't exist!");
+			return;
+		}
+		s.source.Pause();
+	} 
+
+	public void StopAll(){
+		foreach(Sound s in sounds){
+			s.source.Stop();
+		}
+      
+	}
+
+	public float GetMusicTime(string name){
+		foreach(Sound s in sounds){
+			if(s.name == name){
+				return s.source.time;
+			}
+		}
+		return 0;
+	}
+	
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject player;
+    public Vector2 playerPos;
+    public SwipeController swipe;
+    public ArrowsController arrow;
+    public JoystickController joystick;
+    public GameObject arrowsUI;
+    public GameObject shotTutorial;
+    public GameObject swipeTutorial;
+
+    //Pixels Related
+
+    //begin
+    [Range(70,280)]
+    public int totalPixels;
+    [HideInInspector]public int pinkPixels;
+    [HideInInspector]public int bluePixels;
+    [HideInInspector]public int greenPixels;
+    [HideInInspector]public int heartGainPixels;
+
+    //spawning
+    [HideInInspector]public int canSpawnPinkPixels;
+    [HideInInspector]public int canSpawnBluePixels;
+    [HideInInspector]public int canSpawnGreenPixels;
+
+    private int pinkRate;
+    private int blueRate;
+    private int greenRate;
+
+    //current
+    [HideInInspector]public int totalPixelsDestroyed;   
+    [HideInInspector]public int pinkPixelsDestroyed;   
+    [HideInInspector]public int bluePixelsDestroyed;
+    [HideInInspector]public int greenPixelsDestroyed;
+    
+    void Start () {
+        totalPixelsDestroyed = 0;
+        pinkPixelsDestroyed = 0;   
+        bluePixelsDestroyed = 0;
+        greenPixelsDestroyed = 0;
+        //se tiver no pc vai ser joystick
+        if(!Application.isMobilePlatform)
+        {
+       PlayerPrefs.SetString("PlayerMovement", "joyStick");
+        }
+        print("ac" + totalPixels);
+        GeneratingColorRate();
+        if (PlayerPrefs.GetString("PlayerMovement") == "swipe")
+        {
+            swipeTutorial.SetActive(true);
+            shotTutorial.SetActive(true);
+        }
+
+        else if(PlayerPrefs.GetString("PlayerMovement") == "arrows")
+        {
+            shotTutorial.SetActive(true);
+        }
+         
+        totalPixelsDestroyed = 0;
+        AudioManager audio = AudioManager.instance;
+        if(audio != null){
+            audio.StopAll();
+            audio.Play("Glitch 3.0");
+        }
+        player = Singleton.GetInstance.player;
+        playerPos = new Vector2(0,-4);
+
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        { 
+            Instantiate(player, playerPos, Quaternion.identity);
+        }
+
+        InvokeRepeating("UpdateController", 0f, 10f);
+    }
+
+	
+	void UpdateController () {
+
+        if (PlayerPrefs.GetString("PlayerMovement") == "swipe")
+        {
+            arrowsUI.SetActive(false);
+            arrow.enabled = false;
+           joystick.enabled = false;
+            swipe.enabled = true;
+            
+        }
+        else if(PlayerPrefs.GetString("PlayerMovement") == "arrows")
+        {
+            arrowsUI.SetActive(true);
+            swipe.enabled = false; 
+            joystick.enabled = false;
+            arrow.enabled = true;
+            
+        }
+        else
+        {
+            arrowsUI.SetActive(false);
+            arrow.enabled = false;
+            swipe.enabled = false; 
+           joystick.enabled = true;
+        }
+
+    }
+
+    void GeneratingColorRate()
+    {
+        pinkRate =Random.Range(20,46);
+        blueRate =Random.Range(20,46);
+        greenRate = 100 - pinkRate - blueRate;
+
+        canSpawnPinkPixels  = Mathf.CeilToInt(pinkRate * totalPixels / 100);
+        canSpawnBluePixels  = Mathf.CeilToInt(blueRate * totalPixels / 100);
+        canSpawnGreenPixels = Mathf.CeilToInt(greenRate * totalPixels/ 100);
+
+        pinkPixels = canSpawnPinkPixels +1;
+        bluePixels = canSpawnBluePixels +1;
+        greenPixels = canSpawnGreenPixels + 1;
+
+       print("dc" + totalPixels);
+        print("pink " + canSpawnPinkPixels);
+        print("blue " + canSpawnBluePixels);
+        print("green " + canSpawnGreenPixels);
+    }
+}
+    
