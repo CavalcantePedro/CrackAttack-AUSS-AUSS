abcef0963df25f8eacde463c921634d2c83cb271
Bibizarro <tabatchnikrabih@gmail.com>
2019-10-14 19:27:30 -0300

    ColorBlind Transitions (Needs Bug Solving ; Menu > Game doesn't works)

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -8,6 +8,47 @@ public class SceneController : MonoBehaviour
     public Animator anim;
 	public Animator anim2;
 
+    private GameObject activeGlitch;
+
+    void Start()
+    {
+       CheckGlitchColor();
+    }
+
+    public void CheckGlitchColor()
+    {
+        switch(PlayerPrefs.GetInt("ColorBlind"))
+        {
+            case 0:
+            anim = GameObject.Find("Canvas/Glitch/Original_Color/OriginalGlitchUp").GetComponent<Animator>();
+            anim2 = GameObject.Find("Canvas/Glitch/Original_Color/OriginalGlitchDown").GetComponent<Animator>();
+            activeGlitch = GameObject.Find("Canvas/Glitch/Original_Color");
+            activeGlitch.SetActive(true);
+            break;
+            
+            case 1:
+            anim = GameObject.Find("Canvas/Glitch/Monochromatic/MonGlitchUp").GetComponent<Animator>();
+            anim2 = GameObject.Find("Canvas/Glitch/Monochromatic/MonGlitchDown").GetComponent<Animator>();
+            activeGlitch = GameObject.Find("Canvas/Glitch/Monochromatic");
+            activeGlitch.SetActive(true);
+            break;
+            
+            case 2:
+            anim = GameObject.Find("Canvas/Glitch/Dichromatic/DicGlitchUp").GetComponent<Animator>();
+            anim2 = GameObject.Find("Canvas/Glitch/Dichromatic/DicGlitchDown").GetComponent<Animator>();
+            activeGlitch = GameObject.Find("Canvas/Glitch/Dichromatic");
+            activeGlitch.SetActive(true);
+            break;
+
+            case 3:
+            anim = GameObject.Find("Canvas/Glitch/Trichromatic/TricGlitchUp").GetComponent<Animator>();
+            anim2 = GameObject.Find("Canvas/Glitch/Trichromatic/TricGlitchDown").GetComponent<Animator>();
+            activeGlitch = GameObject.Find("Canvas/Glitch/Trichromatic");
+            activeGlitch.SetActive(true);
+            break;
+        }
+    }
+
     public void ChangeScene(string name){
         SceneManager.LoadScene(name);
     }
