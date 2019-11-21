50be9785e521e79ec863b8eea412415eecf26f89
CavalcantePedro <pedro.r.cavalcante.sf@gmail.com>
2019-11-21 09:31:02 -0300

    LevelConfig 100%

--- Assets/Scripts/UI/LevelSelectionBox.cs
+++ Assets/Scripts/UI/LevelSelectionBox.cs
@@ -1,97 +1,124 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.SceneManagement;
 
-public class LevelSelectionBox : MonoBehaviour
-{
-    
-    [Header("Level Numbers")]
-
-    [SerializeField] private Image numberUI;
-    [SerializeField] private Sprite[] numberSprites;
-    [SerializeField] private Animator numberAnim;
-    [SerializeField] private GameObject plusOneBtn;
-    [SerializeField] private GameObject minusOneBtn;
-    [SerializeField] private int levelNumber;
-
-    [Header("Record")]  
-    [SerializeField] private Text recordText;
-    private string recordSt;
-
-    private int recordPlayer;
-
-    [Header("Difficulty")]
-    [SerializeField] private Toggle difficultySwitch;
-    [HideInInspector] public string difficulty;
-    
-
-    void Start(){
-
-        levelNumber = 1;
-        //GameInit();
-        ManagingLevels();
-        CheckingRecord();
-    
-        StartCoroutine(UpdateBox());
-    }
-
-    void UpdateStatus(){
-        numberUI.sprite = numberSprites[levelNumber];
-        numberAnim.SetInteger("TransitionTo", levelNumber);
-    }
-
-    public void SelectNextLevel(){
-        levelNumber++;
-        ManagingLevels();
-        CheckingRecord();
-       // GameInit();
-        StartCoroutine(InteractionSwitch());
-    }
-
-    public void SelectPreviousLevel(){
-        levelNumber--;
-        ManagingLevels();
-        CheckingRecord();
-        //GameInit();
-        StartCoroutine(InteractionSwitch());
-    }
-    
-    /* public void DifficultyChange(){
-        
-        if(difficultySwitch.isOn){
-            difficulty = "hard";
-        }
-
-        else{
-            difficulty = "easy";
-        }
-
-    }*/
-
-    void ManagingLevels()
+    public class LevelSelectionBox : MonoBehaviour
     {
-        switch(levelNumber)
-        {
-            case 1 :
-                minusOneBtn.SetActive(false);
-            break;
+        
+        [Header("Level Numbers")]
 
-            case 4 :
-                plusOneBtn.SetActive(false);
-            break;
+        [SerializeField] private Image numberUI;
+        [SerializeField] private Sprite[] numberSprites;
+        [SerializeField] private Animator numberAnim;
+        [SerializeField] private GameObject plusOneBtn;
+        [SerializeField] private GameObject minusOneBtn;
+        [SerializeField] private int levelNumber;
 
-            default:
-                if(!minusOneBtn.activeSelf || !plusOneBtn.activeSelf){
-                    minusOneBtn.SetActive(true);
-                    plusOneBtn.SetActive(true);
-                }
-            break;
+        [Header("Record")]  
+        [SerializeField] private Text recordText;
+        private string recordSt;
+
+         private int recordPlayer;
+
+        [Header("Difficulty")]
+        [SerializeField] private Toggle difficultySwitch;
+        [HideInInspector] public string difficulty;
+        
+
+        void Start(){
+        // PlayerPrefs.SetInt("LevelsWon" , 1);
+            levelNumber = 1;
+            if(!PlayerPrefs.HasKey("LevelsWon"))
+            {
+                PlayerPrefs.SetInt("LevelsWon" , 1);
+            }
+            //print("Olha aqui " + PlayerPrefs.GetInt("LevelsWon"));
+            //GameInit();
+            ManagingLevels();
+            CheckingRecord();
+        
+            StartCoroutine(UpdateBox());
         }
-    }
 
-    void CheckingRecord()
+        void UpdateStatus(){
+            numberUI.sprite = numberSprites[levelNumber];
+            numberAnim.SetInteger("TransitionTo", levelNumber);
+        }
+
+        public void SelectNextLevel(){
+            levelNumber++;
+            ManagingLevels();
+            CheckingRecord();
+        // GameInit();
+            StartCoroutine(InteractionSwitch());
+        }
+
+        public void SelectPreviousLevel(){
+            levelNumber--;
+            ManagingLevels();
+            CheckingRecord();
+            StartCoroutine(InteractionSwitch());
+        }
+    
+        void ManagingLevels()
+        {
+            switch(levelNumber)
+            {
+                case 1 :
+                    minusOneBtn.SetActive(false);
+                    if(PlayerPrefs.GetInt("LevelsWon") == 1)
+                    {
+                        plusOneBtn.SetActive(false);
+                    }
+                    else
+                    {
+                        plusOneBtn.SetActive(true);
+                    }
+                // print("Olha isso" + levelNumber);
+                break;
+                
+                case 2 :
+                minusOneBtn.SetActive(true);
+                if(PlayerPrefs.GetInt("LevelsWon") == 3)
+                {
+                plusOneBtn.SetActive(true);
+                }
+                else
+                {
+                plusOneBtn.SetActive(false);
+                }
+                break;
+
+                case 3 :
+                minusOneBtn.SetActive(true);
+                if(PlayerPrefs.GetInt("LevelsWon") == 4)
+                {
+                plusOneBtn.SetActive(true);
+                }
+                else
+                {
+                plusOneBtn.SetActive(false);
+                }
+                break;
+
+                case 4 :
+                    plusOneBtn.SetActive(false);
+                //  print("Olha isso" + levelNumber);
+                break;
+
+                default:
+                    if(!minusOneBtn.activeSelf || !plusOneBtn.activeSelf){
+                        minusOneBtn.SetActive(true);
+                        plusOneBtn.SetActive(true);
+                        print("Olha isso" + levelNumber);
+                    }
+                break;
+            }
+        }
+
+       void CheckingRecord()
     {
 
         switch(levelNumber) {
@@ -115,7 +142,7 @@ public class LevelSelectionBox : MonoBehaviour
         ShowingRecord();
     }
 
-    void ShowingRecord() {
+     void ShowingRecord() {
         if(recordPlayer > 0) {
             //recordSt = PlayerPrefs.GetInt(recordPlayerPref).ToString();
             recordText.text ="Record:" + recordPlayer.ToString();
@@ -124,51 +151,50 @@ public class LevelSelectionBox : MonoBehaviour
             recordText.text = "Record:0";
         }
     }
+        public void GameInit(){
 
-    public void GameInit(){
-
-        
-
-        print("Number info (1):" + PlayerPrefs.GetInt("Level"));
-
-         switch(levelNumber){
-            case 1:
-                print("Entrou");
-                 PlayerPrefs.SetInt("Level", levelNumber);
-                SceneManager.LoadScene("Cutscene");
-            break;
             
-            case 2:
-                print("Entrou2");
-                 PlayerPrefs.SetInt("Level", levelNumber);
-                SceneManager.LoadScene("GameA2");
-            break;
 
-            case 3:
-             PlayerPrefs.SetInt("Level", levelNumber);
-               SceneManager.LoadScene("GameA3");
-            break;
+        // print("Number info (1):" + PlayerPrefs.GetInt("Level"));
 
-            case 4:
-             PlayerPrefs.SetInt("Level", levelNumber);
-                SceneManager.LoadScene("GameA4");
-            break;
+            switch(levelNumber){
+                case 1:
+                // print("Entrou");
+                    PlayerPrefs.SetInt("Level", levelNumber);
+                    SceneManager.LoadScene("Cutscene");
+                break;
+                
+                case 2:
+                //  print("Entrou2");
+                    PlayerPrefs.SetInt("Level", levelNumber);
+                    SceneManager.LoadScene("GameA2");
+                break;
+
+                case 3:
+                PlayerPrefs.SetInt("Level", levelNumber);
+                SceneManager.LoadScene("GameA3");
+                break;
+
+                case 4:
+                PlayerPrefs.SetInt("Level", levelNumber);
+                    SceneManager.LoadScene("GameA4");
+                break;
+            }
+        }
+
+        IEnumerator InteractionSwitch()
+        {
+            plusOneBtn.GetComponent<Button>().interactable = !plusOneBtn.GetComponent<Button>().interactable;
+            minusOneBtn.GetComponent<Button>().interactable = !minusOneBtn.GetComponent<Button>().interactable;
+            yield return new WaitForSeconds(0.25f);
+            plusOneBtn.GetComponent<Button>().interactable = !plusOneBtn.GetComponent<Button>().interactable;
+            minusOneBtn.GetComponent<Button>().interactable = !minusOneBtn.GetComponent<Button>().interactable;
+        }
+
+        IEnumerator UpdateBox() {
+            for(;;){
+                UpdateStatus();
+                yield return new WaitForSeconds(0.2f);
+            }
         }
     }
-
-    IEnumerator InteractionSwitch()
-    {
-        plusOneBtn.GetComponent<Button>().interactable = !plusOneBtn.GetComponent<Button>().interactable;
-        minusOneBtn.GetComponent<Button>().interactable = !minusOneBtn.GetComponent<Button>().interactable;
-        yield return new WaitForSeconds(0.25f);
-        plusOneBtn.GetComponent<Button>().interactable = !plusOneBtn.GetComponent<Button>().interactable;
-        minusOneBtn.GetComponent<Button>().interactable = !minusOneBtn.GetComponent<Button>().interactable;
-    }
-
-    IEnumerator UpdateBox() {
-        for(;;){
-            UpdateStatus();
-            yield return new WaitForSeconds(0.2f);
-        }
-    }
-}
